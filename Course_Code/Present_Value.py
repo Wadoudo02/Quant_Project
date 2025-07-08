49afd41a2601f37a8f64e39d390e82e32ddffc2b
Wadoudo2002 <wadoudo@icloud.com>
2025-07-08 13:05:56 +0100

    All code created so far

---
+++ Course_Code/Present_Value.py
@@ -0,0 +1,30 @@
+from math import exp
+
+def future_descrete_value(x, r, n):
+    return x * (1+ r)**n
+
+def present_descrete_value(x, r, n):
+    return x * (1+ r)**-n
+
+def future_continuous_value(x, r, t):
+    return x * exp(r*t)
+
+def present_continuous_value(x, r, t):
+    return x * exp(-r*t)
+
+
+
+# Value of investment in $
+
+x = 100
+
+# Interest rate 5%
+
+r = 0.05 
+
+n = 5 # 5 Years
+
+print("Future value of x (discrete): %s" % future_descrete_value(x, r, n))
+print("Present value of x (discrete): %s" % present_descrete_value(x, r, n))
+print("Future value of x (continuous): %s" % future_continuous_value(x, r, n))
+print("Present value of x (continuous): %s" % present_continuous_value(x, r, n))
---
+++ Course_Code/Zero_Coupon_Bond.py
@@ -0,0 +1,19 @@
+class ZeroCouponBonds:
+
+    def __init__(self, principle, maturity, interest_rate):
+        # Principle amount
+        self.principle = principle
+        # date to maturity
+        self.maturity = maturity
+        # market interest rate (discounting)
+        self.interest_rate = interest_rate
+
+    def present_value(self, x, n):
+        return x / (1 + self.interest_rate)**n
+    
+    def calculate_price(self):
+        return self.present_value(self.principle, self.maturity)
+    
+
+bond = ZeroCouponBonds(1000, 2, 0.04)
+print("Price of the bond: %.2f" % bond.calculate_price())
